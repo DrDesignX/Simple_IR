d72e41ec3aa20c5f01bfdbf6122f564abf8f184f
MohamadAminS <mr.seadi@gmail.com>
2024-05-05 15:35:53 +0330

    fix

--- app.py
+++ app.py
@@ -1,111 +1,115 @@
-# import os
-# import pandas as pd # type: ignore
-# from utils import stem
-# from utils import calculator
-# from utils import config
-# from utils import search
-# from utils import stopword
-# from utils import gen_ecxel
+import tkinter as tk
+from tkinter import filedialog
+import os
+import pandas as pd # type: ignore
+from utils import stem
+from utils import calculator
+from utils import config
+from utils import search
+from utils import stopword
+from utils import gen_ecxel
 
-# stem_data = []
+stem_data = []
 
-# def parse_text(directory):
-#     documents = []
-#     try:
-#         for filename in os.listdir(directory):
-#             # print(filename)
-#             if not filename.startswith("cran.all") or filename.endswith(".txt"):
-#                 continue
-#             path = os.path.join(directory, filename)
-#             with open(path, "r") as file:
-#                 text = file.read()
-#                 documents += text.split(".I")
-#     except FileNotFoundError as e:
-#         print(f"Error: {e}. Please make sure the directory exists.")
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-#     return documents[1:]
-
-
-# def create_word_count_list():
-#     word_count_list = []
-#     for document in stem_data:
-#         word_count = {}
-#         tokens = document.split()
-#         for token in tokens:
-#             token = token.strip('.,?!";:')
-#             word_count[token] = word_count.get(token, 0) + 1
-#         if word_count:
-#             del word_count[next(iter(word_count))]
-#         word_count_list.append(word_count)
-#     return word_count_list
-
-# def generate_matrix(word_doc_list):
-#     all_words = set(stem.get_unique_words(word_doc_list))
-#     num_rows = len(all_words) + 1  # +1 for the header row
-#     num_cols = len(word_doc_list) + 1  # +1 for the "Words" column
-#     matrix = [[0] * num_cols for _ in range(num_rows)]
-
-#     # Populate the header row
-#     matrix[0][0] = "Words"
-#     for col in range(1, num_cols):
-#         matrix[0][col] = f"Document {col}"
-#     # Populate the word counts
-#     for row, word in enumerate(all_words, start=1):
-#         matrix[row][0] = word
-#         for col, word_count in enumerate(word_doc_list, start=1):
-#             count = word_count.get(word, 0)
-#             matrix[row][col] = count
-#     return matrix
-
-# def main():
-#     directory = config.DATA_URL
-#     stopwords = stopword.load()
-#     documents = parse_text(directory)
-#     for document in documents:
-#         stem_data.append(stem.process_text(document, stopwords))
-#     word_count_list = create_word_count_list()
-
-#     unique_words_set = set()
-
-#     # Create a dictionary to store word counts
-#     word_counts = {}
-
-#     for dic in word_count_list:
-#         for word, count in dic.items():
-#             # Add word to set
-#             unique_words_set.add(word)
-#             # Add word to dictionary with its count
-#             word_counts[word] = count
-#             # Print word and its count
-#             print(f"{word}, Count: {count}")
+def parse_text(directory):
+    documents = []
+    try:
+        for filename in os.listdir(directory):
+            # print(filename)
+            if not filename.startswith("cran.all") or filename.endswith(".txt"):
+                continue
+            path = os.path.join(directory, filename)
+            with open(path, "r") as file:
+                text = file.read()
+                documents += text.split(".I")
+    except FileNotFoundError as e:
+        print(f"Error: {e}. Please make sure the directory exists.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return documents[1:]
 
 
-#     word_df = pd.DataFrame(word_count_list)
-#     # print(word_df.head())
-#     # word_df.to_excel("word_data.xlsx", index=False)
+def create_word_count_list():
+    word_count_list = []
+    for document in stem_data:
+        word_count = {}
+        tokens = document.split()
+        for token in tokens:
+            token = token.strip('.,?!";:')
+            word_count[token] = word_count.get(token, 0) + 1
+        if word_count:
+            del word_count[next(iter(word_count))]
+        word_count_list.append(word_count)
+    return word_count_list
 
-#     unique_words_list = stem.get_unique_words(word_count_list)
-#     num_unique_words = len(unique_words_list)
-#     print("Number of words:", num_unique_words)
-#     printer=False
+def generate_matrix(word_doc_list):
+    all_words = set(stem.get_unique_words(word_doc_list))
+    num_rows = len(all_words) + 1  # +1 for the header row
+    num_cols = len(word_doc_list) + 1  # +1 for the "Words" column
+    matrix = [[0] * num_cols for _ in range(num_rows)]
 
-#     if printer:
-#         term_document_matrix = generate_matrix(word_count_list)
-#         term_document_matrix_df = pd.DataFrame(term_document_matrix[1:], columns=term_document_matrix[0])
-#         gen_ecxel.gen(term_document_matrix_df, "raw-data.xlsx")
+    # Populate the header row
+    matrix[0][0] = "Words"
+    for col in range(1, num_cols):
+        matrix[0][col] = f"Document {col}"
+    # Populate the word counts
+    for row, word in enumerate(all_words, start=1):
+        matrix[row][0] = word
+        for col, word_count in enumerate(word_doc_list, start=1):
+            count = word_count.get(word, 0)
+            matrix[row][col] = count
+    return matrix
 
-#         idf_values_df = calculator.idf(term_document_matrix)
-#         idf_df = pd.DataFrame(idf_values_df.items(), columns=["Word", "IDF"])
-#         gen_ecxel.gen(idf_df, "idf-data.xlsx")
+def main():
+    def select_folder():
+        folder_path = filedialog.askdirectory()
+        config.DATA_URL = folder_path
+        folder_path_var.set(folder_path)
+        root.destroy()
 
-#         tf_matrix = calculator.tf(term_document_matrix)
-#         tf_matrix_df = pd.DataFrame(tf_matrix[1:], columns=tf_matrix[0])
-#         gen_ecxel.gen(tf_matrix_df, "tf-data.xlsx")
+    root = tk.Tk()
+    root.title("Choose a folder")
 
-#         tf_idf_matrix = calculator.tf_idf(term_document_matrix, idf_values)
-#         tf_idf_matrix_df = pd.DataFrame(tf_idf_matrix[1:], columns=tf_idf_matrix[0])
-#         gen_ecxel.gen(tf_idf_matrix_df, "tf-idf-data.xlsx")
+    window_width = 400
+    window_height = 200
+    root.geometry(f"{window_width}x{window_height}")
+    folder_path_var = tk.StringVar()
 
-# if __name__ == "__main__":
-#     main()
+    folder_label = tk.Label(root, textvariable=folder_path_var)
+    folder_label.pack()
+    select_button = tk.Button(root, text="Browse", command=select_folder)
+    select_button.pack()
+    root.mainloop()
+
+    directory = config.DATA_URL
+    print(directory)
+    stopwords = stopword.load()
+    documents = parse_text(directory)
+    for document in documents:
+        stem_data.append(stem.process_text(document, stopwords))
+    word_count_list = create_word_count_list()
+
+    unique_words_list = stem.get_unique_words(word_count_list)
+    num_unique_words = len(unique_words_list)
+    print("Number of words:", num_unique_words)
+    printer=True
+
+
+    term_document_matrix = generate_matrix(word_count_list)
+    term_document_matrix_df = pd.DataFrame(term_document_matrix[1:], columns=term_document_matrix[0])
+    gen_ecxel.gen(term_document_matrix_df, "raw-data.xlsx")
+
+    idf_values_df = calculator.idf(term_document_matrix)
+    idf_df = pd.DataFrame(idf_values_df.items(), columns=["Word", "IDF"])
+    gen_ecxel.gen(idf_df, "idf-data.xlsx")
+
+    tf_matrix = calculator.tf(term_document_matrix)
+    tf_matrix_df = pd.DataFrame(tf_matrix[1:], columns=tf_matrix[0])
+    gen_ecxel.gen(tf_matrix_df, "tf-data.xlsx")
+
+    tf_idf_matrix = calculator.tf_idf(term_document_matrix, idf_values)
+    tf_idf_matrix_df = pd.DataFrame(tf_idf_matrix[1:], columns=tf_idf_matrix[0])
+    gen_ecxel.gen(tf_idf_matrix_df, "tf-idf-data.xlsx")
+
+if __name__ == "__main__":
+    main()
--- app2.py
+++ app2.py
@@ -1,124 +1,134 @@
-import os
-import pandas as pd
-from utils import stem, calculator, config, stopword, gen_ecxel
-from flask import Flask, jsonify,render_template,request  # type: ignore
-from werkzeug.utils import secure_filename
-import os
+# import os
+# import pandas as pd
+# from utils import stem, calculator, config, stopword, gen_ecxel
+# from flask import Flask, jsonify,render_template,request ,send_from_directory # type: ignore
+# # from werkzeug.utils import secure_filename
+# import os
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('view/index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-UPLOAD_FOLDER = 'uploads'
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return 'No file has been selected!'
+# global URL
 
-    file = request.files['file']
+# app = Flask(__name__)
 
-    if file.filename == '':
-        return 'No file selected!'
-        
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(UPLOAD_FOLDER, filename)
-    
-    # Save the file to the desired location
-    file.save(file_path)
-   
-    return 'File successfully uploaded! File path: {}'.format(file_path)
+# @app.route('/upload', methods=['POST', 'GET'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # Check if a file was uploaded
+#         if 'file' not in request.files:
+#             return 'No file part'
 
+#         file = request.files['file']
 
-@app.route('/word_count', methods=['GET'])
-def get_word_count():
-    directory = config.DATA_URL
-    stopwords = stopword.load()
-    documents = parse_text(directory)
-    stem_data = [stem.process_text(document, stopwords) for document in documents]
-    word_count_list = create_word_count_list(stem_data)
-    return jsonify(word_count_list)
+#         # Check if the file is selected
+#         if file.filename == '':
+#             return 'No selected file'
 
-@app.route('/tf_matrix', methods=['GET'])
-def get_tf_matrix():
-    directory = config.DATA_URL
-    stopwords = stopword.load()
-    documents = parse_text(directory)
-    stem_data = [stem.process_text(document, stopwords) for document in documents]
-    word_count_list = create_word_count_list(stem_data)
-    term_document_matrix = generate_matrix(word_count_list)
-    tf_matrix = calculator.tf(term_document_matrix)
-    return jsonify(tf_matrix)
-
-@app.route('/idf_matrix', methods=['GET'])
-def get_tf_matrix():
-    directory = config.DATA_URL
-    stopwords = stopword.load()
-    documents = parse_text(directory)
-    stem_data = [stem.process_text(document, stopwords) for document in documents]
-    word_count_list = create_word_count_list(stem_data)
-    term_document_matrix = generate_matrix(word_count_list)
-    idf_matrix = calculator.idf(term_document_matrix)
-    return jsonify(idf_matrix)
+#         try:
+#             # Save the uploaded file to the specified folder
+#             file.save('data/uploads/' + file.filename)
+#             return 'File uploaded successfully: ' + file.filename
+#         except Exception as e:
+#             return 'Error uploading file: ' + str(e)
+#     else:
+#         return 'Upload file here'
 
 
-@app.route('/tf_idf_matrix', methods=['GET'])
-def get_tf_idf_matrix():
-    directory = config.DATA_URL
-    stopwords = stopword.load()
-    documents = parse_text(directory)
-    stem_data = [stem.process_text(document, stopwords) for document in documents]
-    word_count_list = create_word_count_list(stem_data)
-    term_document_matrix = generate_matrix(word_count_list)
-    idf_values = calculator.idf(term_document_matrix)
-    tf_idf_matrix = calculator.tf_idf(term_document_matrix, idf_values)
-    return jsonify(tf_idf_matrix)
+# @app.route('/word_count', methods=['GET'])
+# def get_word_count():
+#     directory = config.DATA_URL
+#     stopwords = stopword.load()
+#     documents = parse_text(directory)
+#     stem_data = [stem.process_text(document, stopwords) for document in documents]
+#     word_count_list = create_word_count_list(stem_data)
+#     return jsonify(word_count_list)
 
-def parse_text(directory):
-    documents = []
-    try:
-        for filename in os.listdir(directory):
-            if not filename.startswith("cran.all") or filename.endswith(".txt"):
-                continue
-            path = os.path.join(directory, filename)
-            with open(path, "r") as file:
-                text = file.read()
-                documents += text.split(".I")
-    except FileNotFoundError as e:
-        print(f"Error: {e}. Please make sure the directory exists.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    return documents[1:]
+# # @app.route('/tf_matrix', methods=['GET'])
+# # def get_tf_matrix():
+# #     directory = URL
+# #     stopwords = stopword.load()
+# #     documents = parse_text(directory)
+# #     stem_data = [stem.process_text(document, stopwords) for document in documents]
+# #     word_count_list = create_word_count_list(stem_data)
+# #     term_document_matrix = generate_matrix(word_count_list)
+# #     tf_matrix = calculator.tf(term_document_matrix)
+# #     return jsonify(tf_matrix)
 
-def create_word_count_list(stem_data):
-    word_count_list = []
-    for document in stem_data:
-        word_count = {}
-        tokens = document.split()
-        for token in tokens:
-            token = token.strip('.,?!";:')
-            word_count[token] = word_count.get(token, 0) + 1
-        if word_count:
-            del word_count[next(iter(word_count))]
-        word_count_list.append(word_count)
-    return word_count_list
+# # @app.route('/idf_matrix', methods=['GET'])
+# # def get_tf_matrix():
+# #     directory = URL
+# #     stopwords = stopword.load()
+# #     documents = parse_text(directory)
+# #     stem_data = [stem.process_text(document, stopwords) for document in documents]
+# #     word_count_list = create_word_count_list(stem_data)
+# #     term_document_matrix = generate_matrix(word_count_list)
+# #     idf_matrix = calculator.idf(term_document_matrix)
+# #     return jsonify(idf_matrix)
 
-def generate_matrix(word_doc_list):
-    all_words = set(stem.get_unique_words(word_doc_list))
-    num_rows = len(all_words) + 1
-    num_cols = len(word_doc_list) + 1
-    matrix = [[0] * num_cols for _ in range(num_rows)]
-    matrix[0][0] = "Words"
-    for col in range(1, num_cols):
-        matrix[0][col] = f"Document {col}"
-    for row, word in enumerate(all_words, start=1):
-        matrix[row][0] = word
-        for col, word_count in enumerate(word_doc_list, start=1):
-            count = word_count.get(word, 0)
-            matrix[row][col] = count
-    return matrix
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# # @app.route('/tf_idf_matrix', methods=['GET'])
+# # def get_tf_idf_matrix():
+# #     directory = URL
+# #     stopwords = stopword.load()
+# #     documents = parse_text(directory)
+# #     stem_data = [stem.process_text(document, stopwords) for document in documents]
+# #     word_count_list = create_word_count_list(stem_data)
+# #     term_document_matrix = generate_matrix(word_count_list)
+# #     idf_values = calculator.idf(term_document_matrix)
+# #     tf_idf_matrix = calculator.tf_idf(term_document_matrix, idf_values)
+# #     return jsonify(tf_idf_matrix)
+
+# # def parse_text(directory):
+# #     documents = []
+# #     try:
+# #         for filename in os.listdir(directory):
+# #             if not filename.startswith("cran.all") or filename.endswith(".txt"):
+# #                 continue
+# #             path = os.path.join(directory, filename)
+# #             with open(path, "r") as file:
+# #                 text = file.read()
+# #                 documents += text.split(".I")
+# #     except FileNotFoundError as e:
+# #         print(f"Error: {e}. Please make sure the directory exists.")
+# #     except Exception as e:
+# #         print(f"An error occurred: {e}")
+# #     return documents[1:]
+
+# def create_word_count_list(stem_data):
+#     word_count_list = []
+#     for document in stem_data:
+#         word_count = {}
+#         tokens = document.split()
+#         for token in tokens:
+#             token = token.strip('.,?!";:')
+#             word_count[token] = word_count.get(token, 0) + 1
+#         if word_count:
+#             del word_count[next(iter(word_count))]
+#         word_count_list.append(word_count)
+#     return word_count_list
+
+# def generate_matrix(word_doc_list):
+#     all_words = set(stem.get_unique_words(word_doc_list))
+#     num_rows = len(all_words) + 1
+#     num_cols = len(word_doc_list) + 1
+#     matrix = [[0] * num_cols for _ in range(num_rows)]
+#     matrix[0][0] = "Words"
+#     for col in range(1, num_cols):
+#         matrix[0][col] = f"Document {col}"
+#     for row, word in enumerate(all_words, start=1):
+#         matrix[row][0] = word
+#         for col, word_count in enumerate(word_doc_list, start=1):
+#             count = word_count.get(word, 0)
+#             matrix[row][col] = count
+#     return matrix
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
